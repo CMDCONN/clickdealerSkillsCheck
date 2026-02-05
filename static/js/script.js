849a2757d0b4f7dc554253b900515b3929daf425
/dev <88324608+CMDCONN@users.noreply.github.com>
2026-02-05 21:30:02 +0000

    overhaul

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var promo2 = document.getElementById('promo2');
+    if (promo2) {
+        promo2.addEventListener('click', function () {
+            let promo2= "Focus Formula";
+            console.log(promo2);
+        });
+    }
+});
